ff2d4d974a3a1844e21dcd47f2afa44c74e83f39
Jennifer Miller <jmillerarcher@me.com>
2020-02-27 14:53:29 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,17 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
   
+  def new 
+  
+  end
+  
+  def create
+    
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    
+    redirect to posts_path
+    
+  end
+  
+  
 end
